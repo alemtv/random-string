51308a9ca94594d10cee8d8a4afaf99209aea1b3
Aleksandr <turiukompa@gmail.com>
2024-09-30 12:38:44 +0300

    format

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <random>
 using namespace std;
 
-
-std::string generateRandomString(int length) {
+std::string generateRandomString(int length)
+{
     const string characters = "abcdefghijklmnopqrstuvwxyz ";
-    
+
     random_device rd;
     mt19937 generator(rd());
 
@@ -14,18 +14,21 @@ std::string generateRandomString(int length) {
         0, characters.size() - 1);
 
     string random_string;
-    for (int i = 0; i < length; ++i) {
-        random_string
-            += characters[distribution(generator)];
+    for (int i = 0; i < length; ++i)
+    {
+        random_string += characters[distribution(generator)];
     }
 
     return random_string;
 }
 
-
-int main() {
+int main()
+{
     int length = 38;
+
     string randomStr = generateRandomString(length);
+
     cout << "Random String: " << randomStr << std::endl;
+
     return 0;
 }
